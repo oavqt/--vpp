72b0686178715fad655150294a32b6a1a0b41414
oav <ortizqtv@gmail.com>
2022-03-19 14:09:13 -0600

    --App routes --routes assets --routes jest setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-class App extends React.Component {
-  render(): React.ReactNode {
-    return <div>is it working...</div>;
-  }
-}
+const App: FC = () => {
+  return (
+    <BrowserRouter>
+      {/* <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='projects' element={<Projects/>}/>
+      </Routes> */}
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/assets/assets.tsx
@@ -0,0 +1,12 @@
+const assets = {
+  routes: {
+    navigation: {
+      about: { desciption: 'about', path: 'about' },
+      contact: { desciption: 'contact', path: 'contact' },
+      home: { desciption: 'home', path: '/' },
+      projects: { desciption: 'projects', path: 'projects' }
+    }
+  }
+};
+
+export default assets;
